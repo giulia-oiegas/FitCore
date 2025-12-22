9e5a6decc34051f6775f624137d753c59b6052be
Giulia-Nicola Oiegaș <giulia.oiegas@outlook.com>
2025-12-22 13:38:46 +0200

    setup arhitectura initiala: data, web si mobile

---
+++ FitCore/FitCore.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FitCore.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
