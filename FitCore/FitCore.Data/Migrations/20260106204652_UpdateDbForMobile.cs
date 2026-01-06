e135900a808c0f96dc38f286ca1d63bdbc43714c
Giulia-Nicola Oiegaș <giulia.oiegas@outlook.com>
2026-01-06 23:01:03 +0200

    trying to get rid of runtime errors

---
+++ FitCore/FitCore.Data/Migrations/20260106204652_UpdateDbForMobile.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitCore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDbForMobile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
